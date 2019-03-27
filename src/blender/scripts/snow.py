ded1c3830227b3c746228914a8ce8b59ff0bd2d2
Darshit Vora <darshit@quetzal.in>
2019-03-27 18:06:38 +0530

    Removed comments

--- src/blender/scripts/snow.py
+++ src/blender/scripts/snow.py
@@ -1,21 +1,3 @@
-#	OpenShot Video Editor is a program that creates, modifies, and edits video files.
-#   Copyright (C) 2009  Jonathan Thomas
-#
-#	This file is part of OpenShot Video Editor (http://launchpad.net/openshot/).
-#
-#	OpenShot Video Editor is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#
-#	OpenShot Video Editor is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-#	You should have received a copy of the GNU General Public License
-#	along with OpenShot Video Editor.  If not, see <http://www.gnu.org/licenses/>.
-
 
 # Import Blender's python API.  This only works when the script is being
 # run from the context of Blender.  Blender contains it's own version of Python
--- src/language/generate_translations.py
+++ src/language/generate_translations.py
@@ -2,8 +2,7 @@
 """ 
  @file
  @brief This file updates the OpenShot.POT (language translation template) by scanning all source files.
- @author Jonathan Thomas <jonathan@openshot.org>
- 
+
  This file helps you generate the POT file that contains all of the translatable
  strings / text in OpenShot.  Because some of our text is in custom XML files,
  the xgettext command can't correctly generate the POT file.  Thus... the 
@@ -27,26 +26,7 @@
  Command to combine the 2 pot files into 1 file
        $ msgcat ~/openshot/locale/OpenShot/OpenShot_source.pot ~/openshot/openshot/locale/OpenShot/OpenShot_glade.pot -o ~/openshot/main/locale/OpenShot/OpenShot.pot
 
- @section LICENSE
- 
- Copyright (c) 2008-2018 OpenShot Studios, LLC
- (http://www.openshotstudios.com). This file is part of
- OpenShot Video Editor (http://www.openshot.org), an open-source project
- dedicated to delivering high quality video editing and animation solutions
- to the world.
- 
- OpenShot Video Editor is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- OpenShot Video Editor is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with OpenShot Library.  If not, see <http://www.gnu.org/licenses/>.
+
  """
 
 import shutil
